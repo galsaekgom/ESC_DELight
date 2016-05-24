88dd5653ef4ed26df21f980335dc6932b6e93225
furapan <furapan@gmailc.om>
2016-05-24 23:42:10 +0900

    dummy image 추가

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ function drawMenu(data){
         .append('<div class="menu">'+
                 '<div class="store">'+data.store+'</div>' +
                 '<div class="name">'+data.name+'</div>' +
+                '<div class="img"><img src="http://dummyimage.com/100x70/a3a3a3/ffffff&text=no+image"/></div>' +
                 '<div class="cal">'+data.cal+'kcal</div>' +
                 '</div>')
 }
