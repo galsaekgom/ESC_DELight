6707fda111bf996c890a4d4e4e0b19423e26bf3f
furapan <furapan@gmailc.om>
2016-05-27 01:48:11 +0900

    bootstrap, jquery lib 추가 bootstrap 간단 적용

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ $(function(){
 
 
 function drawMenu(data){
-    $('section.menus')
+    $('.menulist')
         .append('<div class="menu row">'+
-            '<div class="store col-md-12">'+data.store+'</div>' +
+            '<div class="store col-md-12"><h3>'+data.store+'</h3></div>' +
             '<div class="img col-md-4"><img src="http://dummyimage.com/100x70/a3a3a3/ffffff&text=no+image"/></div>'+
             '<div class="name col-md-4">'+data.name+'</div>' +
             '<div class="cal col-md-4">'+data.cal+'kcal</div>' +
