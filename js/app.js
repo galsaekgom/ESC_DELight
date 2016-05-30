5ef9360e98ccfad73b23838539ca0b91f7afd99c
furapan <furapan@gmailc.om>
2016-05-30 21:53:22 +0900

    edit layout

--- js/app.js
+++ js/app.js
@@ -12,8 +12,7 @@ function drawMenu(data){
     $('.menulist')
         .append('<div class="menu row">'+
             '<div class="store col-md-12"><h3>'+data.store+'</h3></div>' +
-            '<div class="img col-md-4"><img src="http://dummyimage.com/100x70/a3a3a3/ffffff&text=no+image"/></div>'+
-            '<div class="name col-md-4">'+data.name+'</div>' +
-            '<div class="cal col-md-4">'+data.cal+'kcal</div>' +
+            '<div class="img col-xs-4 col-sm-4"><img src="http://dummyimage.com/100x70/a3a3a3/ffffff&text=no+image"/></div>'+
+            '<div class="name col-xs-6 col-sm-4">'+data.name+'<br/>'+data.cal+'kcal</div>' +
             '</div>')
 }
